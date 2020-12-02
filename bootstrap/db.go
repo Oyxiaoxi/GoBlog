9842d74384acd71337ec65970c57289864fa3bb9
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2020-12-02 23:55:08 +0800

    Refactor Article List

--- bootstrap/db.go
+++ bootstrap/db.go
@@ -1,10 +1,37 @@
 package bootstrap
 
 import (
+	"goblog/pkg/logger"
 	"time"
 
 	"goblog/pkg/model"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+	gormlogger "gorm.io/gorm/logger"
 )
+
+var DB *gorm.DB
+
+// ConnectDB 初始化模型
+func ConnectDB() *gorm.DB {
+
+	var err error
+
+	config := mysql.New(mysql.Config{
+		DSN: "homestead:secret@tcp(127.0.0.1:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
+	})
+
+	// 准备数据库连接池
+	DB, err = gorm.Open(config, &gorm.Config{
+		Logger: gormlogger.Default.LogMode(gormlogger.Info),
+	})
+
+	logger.LogError(err)
+
+	return DB
+}
 
 // SetupDB 初始化数据库和 ORM
 func SetupDB() {
