5f43a41b6c3c68a25d609dda0938bdfd41340995
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2021-03-29 00:31:59 +0800

    Cutting Template

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -1,18 +1,31 @@
 package article
 
 import (
+	"goblog/app/models"
 	"goblog/pkg/route"
-	"strconv"
+	"goblog/pkg/types"
 )
 
 // Article 文章模型
 type Article struct {
-	ID    int
-	Title string
-	Body  string
+	models.BaseModel
+
+	Title string `gorm:"type:varchar(255);not null;" valid:"title"`
+	Body  string `gorm:"type:longtext;not null;" valid:"body"`
+
 }
 
 // Link 方法用来生成文章链接
 func (a Article) Link() string {
-	return route.Name2URL("articles.show", "id", strconv.FormatInt(int64(a.ID), 10))
+	return route.Name2URL("articles.show", "id", a.GetStringID())
+}
+
+// GetStringID 获取 ID 的字符串格式
+func (a Article) GetStringID() string {
+	return types.Uint64ToString(a.ID)
+}
+
+// CreatedAtDate 创建日期
+func (a Article) CreatedAtDate() string {
+	return a.CreatedAt.Format("2006-01-02")
 }
